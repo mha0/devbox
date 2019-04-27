607bf8f89c355cd527a4e1778b63814afcfba352
Matthias Hausherr <mat@localhost.localdomain>
2019-04-27 13:48:14 +0200

    flatpak

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
 sudo dnf upgrade
 sudo dnf install ansible
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
