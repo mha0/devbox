7ed6acaf7ef6bed38a34ba85963d4421f4d9e2d9
Matthias Hausherr <mat@localhost.localdomain>
2019-05-06 16:19:48 +0200

    assumeyes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,11 @@
-sudo dnf upgrade
-sudo dnf install ansible
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+# System upgrade
+sudo dnf upgrade --assumeyes
+
+# Install Ansible
+sudo dnf install ansible --assumeyes
+
+# Enable RPM Fusion repos
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm --assumeyes
+
+# Add flathub repo
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
