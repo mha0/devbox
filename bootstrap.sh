bdf4981989ec2d7581596fb0688c7805392480f6
Matthias Hausherr <matthias.hausherr@protonmail.ch>
2019-05-28 22:28:47 +0200

    snaps

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
 # System upgrade
-sudo apt update
-sudo apt upgrade --yes
+#sudo apt update
+#sudo apt upgrade --yes
 
 # Install Ansible
-sudo apt install ansible --yes
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
