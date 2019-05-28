990045000c4564eede815e2254fb6f0094486005
Matthias Hausherr <matthias.hausherr@protonmail.ch>
2019-05-28 20:55:56 +0200

    bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,4 @@ sudo apt update
 sudo apt upgrade --yes
 
 # Install Ansible
-sudo apt install ansible --assumeyes
-
-# Add flathub repo
-sudo apt install flatpak
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo apt install ansible --yes
