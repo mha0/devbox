72b4cf5b60099840ed583936011b2a90895b2c05
Matthias Hausherr <matthias.hausherr@competec.ch>
2019-05-28 16:31:06 +0200

    DEV-0 Changes for kubuntu

--- ansible/zsh/files/.zshrc
+++ ansible/zsh/files/.zshrc
@@ -38,7 +38,3 @@ alias dckill='dcid | xargs docker stop | xargs docker rm'
 # clipboard
 alias xc="xclip -selection c"
 alias xv="xclip -selection c -o"
-
-# flatpak
-alias subl="flatpak run com.sublimetext.three . &"
-alias peek="flatpak run com.uploadedlobster.peek &"
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,10 @@
 # System upgrade
-sudo dnf upgrade --assumeyes
+sudo apt update
+sudo apt upgrade --yes
 
 # Install Ansible
-sudo dnf install ansible --assumeyes
-
-# Enable RPM Fusion repos
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm --assumeyes
+sudo apt install ansible --assumeyes
 
 # Add flathub repo
+sudo apt install flatpak
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
