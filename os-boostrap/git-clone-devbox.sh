ceedb59246665a8d9a79949fece6939919b22c52
Matthias Hausherr <matthias.hausherr@competec.ch>
2019-06-04 21:56:31 +0200

    DEV-0 wip

--- os-boostrap/git-clone-devbox.sh
+++ os-boostrap/git-clone-devbox.sh
@@ -1,4 +1,11 @@
+# create local git dir
 sudo mkdir /var/git
+
+# create link
 ln -s /var/git/ ~/git
+
+# ssh clone devbox
 git clone git@github.com:mha0/devbox.git ~/git/devbox
 
+# cleanup
+rm -rf ~/devbox
---
+++ os-boostrap/os-bootstrap.sh
@@ -0,0 +1,17 @@
+if [ $# -eq 0 ]
+  then
+    echo "No ssh key name supplied"
+    exit 1
+fi
+
+# system upgrade
+sudo apt update
+sudo apt upgrade --yes
+
+# install packages
+sudo apt install open-vm-tools-desktop
+sudo apt install git
+git config --global user.email "$1"
+
+# generate ssh key
+ssh-keygen -t rsa -b 4096 -C "$1"
