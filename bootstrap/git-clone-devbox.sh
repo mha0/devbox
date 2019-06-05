bef0308a3337bb5e6646959af5fcea99fe7cf6fb
Matthias Hausherr <matthias.hausherr@competec.ch>
2019-06-05 14:01:03 +0200

    DEV-0 wip

--- bootstrap/git-clone-devbox.sh
+++ bootstrap/git-clone-devbox.sh
@@ -3,7 +3,7 @@ sudo mkdir /var/git/
 sudo chown mat:mat /var/git/
 
 # create link
-sudo ln -s /var/git/ ~/git
+ln -s /var/git/ ~/git
 
 # ssh clone devbox
 git clone git@github.com:mha0/devbox.git ~/git/devbox
