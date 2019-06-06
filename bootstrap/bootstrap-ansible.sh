5467edf50d1e26192833b4a62bf261480bb764d6
Matthias Hausherr <ubuntu-devbox@tim>
2019-06-06 21:25:52 +0200

    DEV-0 wip

--- bootstrap/bootstrap-ansible.sh
+++ bootstrap/bootstrap-ansible.sh
@@ -6,7 +6,10 @@ sudo apt-get --yes upgrade
 sudo apt-get --yes install aptitude 
 sudo apt-get --yes install python-apt 
 
-# install Ansible
+# install ansible
 sudo apt-get --yes install software-properties-common 
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get --yes install ansible
+
+# install ansible roles
+ansible-galaxy install --roles-path ~/.ansible/roles jaredhocutt.gnome_extensions
---
+++ update-extensions.sh
@@ -0,0 +1,1 @@
+ansible-playbook -i ansible/devbox.yml --connection=local ansible/playbook-gnome-extensions.yml
